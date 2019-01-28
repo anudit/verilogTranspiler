08bfe8a8f74dd52b508a0aaf4d5cace5be8a5a40
Anudit Nagar <nagaranudit@gmail.com>
2019-01-28 23:53:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+MOD_NAME = "main"
+boolexp = "~AD + ~CD + A~BD"
+
+verilog = ""
+mainVars = []
+boolexp = boolexp.replace(" ", "")
+boolexpList = boolexp.split('+')
+
+wCnt = 1;
+for ele in boolexpList:
+    expStack = []
+    for ind in range(len(ele)):
+        if(ele[ind] == "~"):
+            mainVars.append(ele[ind+1])
+            expStack.append("~" + ele[ind+1] )
+            ind+=1
+        elif(ele[ind].isalpha()):
+            mainVars.append(ele[ind])
+            expStack.append(ele[ind] )
+    verilogGate = "\tand(" + "w" + str(wCnt)
+    for exp in expStack:
+        verilogGate +=  "," + str(exp)
+    verilogGate += ");\n"
+    # print(verilogGate)
+    verilog += verilogGate
+    wCnt+=1
+
+mainVars = sorted(list(set(mainVars)))
+# print(mainVars)
+orGate = "\tor(o"
+for n in range(1, wCnt):
+    orGate += ",w" + str(n)
+orGate += ");\n"
+verilog += orGate   
+
+wireLine = "\twire "
+for inp in range(1, wCnt):
+    wireLine += "w" + str(inp)
+    if (inp == wCnt-1):
+        wireLine += ";"
+    else:
+        wireLine += ","
+wireLine += "\n"
+
+moduleLine = "module " + MOD_NAME + "("
+inputLine = "\tinput "
+for inp in range(len(mainVars)):
+    moduleLine += str(mainVars[inp]) + ","
+    inputLine += str(mainVars[inp])
+    if (inp == len(mainVars)-1):
+        inputLine += ";"
+    else:
+        inputLine += ","
+moduleLine += "o);\n"
+inputLine += "\n"
+
+verilog = moduleLine + inputLine + wireLine + verilog + "endmodule"
+print(verilog)
